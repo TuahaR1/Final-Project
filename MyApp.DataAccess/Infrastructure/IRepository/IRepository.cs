e4d99b5748915938b6261a1af55c70e77eb66eed
Tuaha Rasool <92690142+TuahaR1@users.noreply.github.com>
2024-12-12 17:38:04 +0500

    Implementation of the Repository Pattern in an Inventory Management System (IMS) to abstract data access logic. This design promotes separation of concerns, scalability, and maintainability by managing CRUD operations for inventory entities through a unified, reusable interface.

---
+++ MyApp.DataAccess/Infrastructure/IRepository/IRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.DataAccess.Infrastructure.IRepository
+{
+
+    // this interface contains the function that controller is performing.
+    public interface IRepository<T>
+    {
+        // getting all records
+         IEnumerable<T> GetAll();
+
+        //finding 
+        T GetT(Expression<Func<T, bool>> predicate);
+
+        // inserting 
+        void Add(T entity);
+
+        //deleting 
+        void Del(T entity);
+
+        //deleting by range
+        void DeleteRange(IEnumerable<T> entity);
+
+    }
+}
